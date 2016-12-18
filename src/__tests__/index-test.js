1beafe4a3b764dcfcf0a3eb7341333ea2773be86
Jerko Steiner <jerko.steiner@nyumc.org>
2016-12-19 00:27:35 +0100

    Add render and pureRender functionality

---
+++ src/__tests__/index-test.js
@@ -0,0 +1,64 @@
+const index = require('../index.js');
+
+describe('index', () => {
+
+  it('exports react, react-dom and test-utils', () => {
+    const React = require('react');
+    const ReactDOM = require('react-dom');
+    const TestUtils = require('react-addons-test-utils');
+    const TestRenderer = require('react-test-renderer');
+    expect(index.React).toBe(React);
+    expect(index.ReactDOM).toBe(ReactDOM);
+    expect(index.TestUtils).toBe(TestUtils);
+    expect(index.TestRenderer).toBe(TestRenderer);
+  });
+
+  describe('rendering', () => {
+    let Component;
+    beforeEach(() => {
+      const React = index.React;
+      Component = React.createClass({
+        render() {
+          return (
+            <div className={this.props.className}>
+              {this.props.children}
+            </div>
+          );
+        }
+      });
+    });
+
+    describe('render', () => {
+
+      it('renders a component def and exports the component and node', () => {
+        const React = index.React;
+        const obj = index.render(
+          <Component className="my-class">test</Component>
+        );
+        expect(obj.component.props.className).toBe('my-class');
+        expect(obj.node).toBeTruthy();
+      });
+
+    });
+
+    describe('pureRender', () => {
+
+      it('renders a component using test renderer', () => {
+        const React = index.React;
+        const obj = index.pureRender(
+          <Component className="my-class">test</Component>
+        );
+        expect(obj.toJSON()).toEqual({
+          "children": ["test"],
+          "props": {
+            "className": "my-class"
+          },
+          "type": "div"
+        });
+      });
+
+    });
+
+  });
+
+});
---
+++ src/index.js
@@ -0,0 +1,23 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const TestUtils = require('react-addons-test-utils');
+const TestRenderer = require('react-test-renderer');
+
+function pureRender() {
+  return TestRenderer.create.apply(TestRenderer, arguments);
+}
+
+function render(c) {
+  const component = TestUtils.renderIntoDocument(c);
+  const node = ReactDOM.findDOMNode(component);
+  return { node, component };
+}
+
+module.exports = {
+  React,
+  ReactDOM,
+  TestUtils,
+  TestRenderer,
+  pureRender,
+  render
+};
